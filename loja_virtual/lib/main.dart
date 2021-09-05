06993d3e3519d8926c820bdfe424dca592f7499d
Vagner Paulino Pinto <46845953+vagnerPaulino@users.noreply.github.com>
2021-09-05 14:01:30 -0300

    End project

---
+++ loja_virtual/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/base_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Loja do Vagner',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BaseScreen(),
+    );
+  }
+}
